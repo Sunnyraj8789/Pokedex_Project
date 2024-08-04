f7a4b1099643d9c16f958d1b1aa41436a4c8d139
Sunny Kumar <sunnyraj3600702@gmail.com>
2024-08-04 09:25:00 +0530

    basic pokedex setup done

---
+++ src/components/Pokemon/Pokemon.jsx
@@ -0,0 +1,14 @@
+function Pokemon({name,image}){
+
+    return(
+        <div>
+            <div>{name}</div>
+            <div><img src={image} /></div>
+        </div>
+    )
+
+
+
+}
+
+export default Pokemon;
