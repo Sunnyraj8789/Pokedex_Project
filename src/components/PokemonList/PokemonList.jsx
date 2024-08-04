7beace1cfdd7ff4b22996b8856c1c0cc53e4d8d0
Sunny Kumar <sunnyraj3600702@gmail.com>
2024-08-04 10:24:44 +0530

    Added pagination

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -7,13 +7,19 @@ function PokemonList(){
     const [pokemonList,setPokemonList]=useState([]);
     const [isLoading,setIsLoading]=useState(true);
 
-    const POKEDEX_URL='https://pokeapi.co/api/v2/pokemon';
+    const [pokedexUrl,setPokedexUrl]=useState('https://pokeapi.co/api/v2/pokemon');
+
+    const [nextUrl,setNextUrl]=useState('');
+    const [prevUrl,setPrevUrl]=useState('');
 
    async function downloadPokemons(){
-    const response=await axios.get(POKEDEX_URL);//download the 20 pokemmon
+    setIsLoading(true);
+    const response=await axios.get(pokedexUrl);//download the 20 pokemmon
 
     const pokemonResults=response.data.results;//we get tge array of pokemons from result
     console.log(response.data);
+    setNextUrl(response.data.next);
+    setPrevUrl(response.data.previous);
     //itrating over the array of pokemons, and using their url, to create an array of promises
     //that will download those 20 pokemons
    const pokemonResultPromise =pokemonResults.map((pokemon)=>axios.get(pokemon.url));
@@ -45,34 +51,21 @@ function PokemonList(){
 
     }
 
-    useEffect(async()=>{
-        downloadPokemons();
-       
-    },[]);
-
-   
-
+    useEffect(()=>{
+        downloadPokemons(); 
+    },[pokedexUrl]);
     return (
         <div className="pokemon-list-wrapper">
-
-
             <div className="pokemon-wrapper">
             {(isLoading)? 'Loading.....':
                  pokemonList.map((p) => <Pokemon name={p.name} image={p.image} key={p.id}/>)
-            
             }
-
             </div>
-
             <div className="controls">
-                <button>Prev</button>
-                <button>Next</button>
+                <button disabled={prevUrl==null} onClick={()=>setPokedexUrl(prevUrl)}>Prev</button>
+                <button disabled={nextUrl==null} onClick={()=> setPokedexUrl(nextUrl)}>Next</button>
             </div>
         </div>
     )
-
-
 }
-
-
 export default PokemonList;
